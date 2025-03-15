aa5ddef070710c1ede9a46beae4cb76c624fbb07
Károly Török <torok.karoly.krisztian@gmail.com>
2025-03-15 17:34:34 +0100

    Show diagnostics on hover

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -22,3 +22,14 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
     vim.opt.relativenumber = false
   end,
 })
+
+-- Show diagnostics on hover
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+  group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+  callback = function()
+    vim.diagnostic.open_float(nil, {
+      focus = false,
+      border = "rounded",
+    })
+  end,
+})
