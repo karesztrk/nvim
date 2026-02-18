285c14d8bffdd5928d4c8a1e177a8dda5456e7b9
Károly Török <torok.karoly.krisztian@gmail.com>
2026-02-18 15:41:40 +0100

    Omitted padding from mini.surround

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -4,13 +4,20 @@ vim.pack.add({
 
 -- mini.surround configuration
 require("mini.surround").setup({
+  -- Solve the extra space issue around the brackets
+  custom_surroundings = {
+    ['('] = { output = { left = '(', right = ')' } },
+    ['['] = { output = { left = '[', right = ']' } },
+    ['{'] = { output = { left = '{', right = '}' } },
+    ['<'] = { output = { left = '<', right = '>' } },
+  },
   mappings = {
     -- Using <leader> is easier
     add = "<leader>sa",       -- Add surrounding in Normal and Visual modes
     delete = "<leader>sd",    -- Delete surrounding
     find = "gsf",             -- Find surrounding (to the right)
     find_left = "gsF",        -- Find surrounding (to the left)
-    highlight = "gsh",        -- Highlight surrounding
+    highlight = "<leader>sh", -- Highlight surrounding
     replace = "<leader>sc",   -- Change surrounding
     update_n_lines = "gsn",   -- Update `n_lines`
   },
--- lua/plugins/nu_disco.lua
+++ lua/plugins/nu_disco.lua
@@ -1,2 +1,2 @@
-vim.pack.add({"https://github.com/karesztrk/nu_disco.nvim.git"})
-vim.cmd[[colorscheme nu_disco]]
+vim.pack.add({ "https://github.com/karesztrk/nu_disco.nvim.git" })
+vim.cmd [[colorscheme nu_disco]]
